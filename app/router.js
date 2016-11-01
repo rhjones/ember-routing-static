9fd4b633d67e5d882ed7c9f64978b8bc50d77f01
rebekahheacock <rebekah.heacock@gmail.com>
2016-11-01 10:22:19 -0400

    Add templates and routes for contact and team
    Use ember g template to create templates for Contact and Team
    Manually add routes to app/router.js
    Add links in main template

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = Ember.Router.extend({
 Router.map(function () {
   this.route('about');
   this.route('portfolio');
+  this.route('team');
+  this.route('contact');
 });
 
 export default Router;
